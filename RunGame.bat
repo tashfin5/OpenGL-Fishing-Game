bb821369741d9d49bc816a68a400bfbe1cc32d97
Miftahul Islam Tashfin <miftahul.islam.tashfin@gmail.com>
2026-06-15 22:52:50 +0600

    Update RunGame.bat to automatically open Code::Blocks project if exe is missing

--- RunGame.bat
+++ RunGame.bat
@@ -7,7 +7,7 @@ if exist "bin\Debug\OpenGL-Fishing-Game.exe" (
     echo Starting the game...
     start "" "bin\Release\OpenGL-Fishing-Game.exe"
 ) else (
-    echo Executable not found! 
-    echo Please build the project in Code::Blocks first before running.
-    pause
+    echo Executable not found! Opening the Code::Blocks project...
+    echo Please build the project (Ctrl+F9) and run it.
+    start "" "OpenGL-Fishing-Game.cbp"
 )
